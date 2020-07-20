2b0d08d6902eb3e181a42975f7158745fd1105ef
Atharv Joshi <joshiatharv1078@protonmail.com>
2020-07-20 16:29:16 +0530

    generalized covid 19 code

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-#import pyaudio
 import os
-#import time
 from gtts import gTTS
 import playsound
 import speech_recognition as sr
-#import sounddevice
 import random
 import sys
-#import smtplib
 import webbrowser
 from selenium import webdriver
 import subprocess
 from covid import Covid
 import regex as re
-
-
+import time
+import wikipedia
+import datetime 
+import pyjokes
+import wikiquotes
 
 
 chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
@@ -46,7 +45,7 @@ def get_audio():
 		said = ""
 
 		try:
-			said = r.recognize_google(audio)
+			said = r.recognize_google(audio , language = "en-in")
 			print(said)
 		except Exception as e:
 			print("Exception" + str(e))
@@ -55,56 +54,68 @@ def get_audio():
 			
 
 def assistant(command):
-	if command in ["stop","terminate"]:
+	if re.search(".*terminate|stop|bye|see you later|rest|deactivate.*",command):
 		talk_to_me("Ok")
 		sys.exit()
 
-	#if "google" in command:
-	elif command in ["browse the net","browse the internet"]:
-		talk_to_me("What should i search for?")
+	elif 'the time' in command:
+	        strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+	        talk_to_me(f"The time is {strTime}")
 
+
+	#elif 'wikipedia' in command:                                    #12345678
+	elif re.search("tell me about|i want to know about",command):        
+	        #command = command.replace("wikipedia", "")
+	        #command = command.split(" ")[-1]
+	        print("inside")
+	        command = re.findall("tell me about|i want to know about ([a-zA-Z0-9])",command)
+	        #print(command)
+	        if len(command) > 1:
+	        	command = " ".join(command)
+	        #	print(command)
+	        results = wikipedia.summary(command, sentences=2)
+	        talk_to_me("According to Wikipedia" + results)
+	        #print(results)
+	        #talk_to_me(results)
+	      
+	elif 'joke' in command:
+	        talk_to_me(pyjokes.get_joke())
+	        
+	        
+	elif "where is" in command:                                  #12345678
+	    command=command.replace("where is","")
+	    location = command
+	    talk_to_me("User asked to Locate"+location)
+
+	    webbrowser.open("https://www.google.nl/maps/place/" + location + "")
+	    
+	    
+	    
+	elif 'wiki' in command:
+	    talk_to_me(wikiquotes.random_quote("gandhi", "english"))
+	    
+	elif 'news' in command:
+	    webbrowser.open("https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en")
+	    
+	elif "country" in command:
+	    command=command.split(" ")
+	    name=CountryInfo(command[1])
+	    talk_to_me(name.capital())                	
+
+	elif("open website" in command):
+		command = command.split(" ")
+		webbrowser.open(url.format(command[2]))
 		
-		command = get_audio()
-		while(True):
-			if(command in ["backup","back up"]):
-				talk_to_me("ok")
-				break
-			elif len(command) == 1:
-				
-				webbrowser.get("chrome").open(url.format(command))
 
-			
-
-
-			else:
-				#webbrowser.get("chrome").open(command)
-				if "new tab" in command:
-					talk_to_me("ok")
-					body = driver.findElement(By.cssSelector("body"))
-					body.send_keys(Keys.CONTROL + 't')
-				else:	
-					talk_to_me(f"command recieved:{command}")
-					driver = webdriver.Chrome()
-					driver.get("https://www.google.com/search?q="+ command)
-					
-
-			talk_to_me("Ready for your next command")
-			command = get_audio()		
-		
-			talk_to_me(f"command recieved:{command}")
-		
-	elif command in ["open a program" , "execute a program"]:
-		talk_to_me("Which one?")
-		command = get_audio()
-		if command == "notepad":
-			subprocess.Popen("C:Windows\\system32\\notepad.exe")
-		if command == "calculator":
-			subprocess.Popen("C:Windows\\system32\\calc.exe")	
-		if command == "sublime text":
-			subprocess.Popen("C:\\Program Files\\Sublime Text 3\\sublime_text.exe")
+	elif re.search(".*notepad.*",command):
+		subprocess.Popen("C:Windows\\system32\\notepad.exe")
+	elif re.search(".*calculator.*",command):
+		subprocess.Popen("C:Windows\\system32\\calc.exe")	
+	elif re.search(".*sublimetext.*",command):
+		subprocess.Popen("C:\\Program Files\\Sublime Text 3\\sublime_text.exe")
 		
 
-	elif "make a note" in command:
+	elif re.search(".*note.*",command):
 		talk_to_me("PLease give a suitable filename")
 		command = get_audio()
 		f_txt = command+".txt"
@@ -115,25 +126,42 @@ def assistant(command):
 		talk_to_me("please check if it's  correct.")
 		x = subprocess.Popen(["C:Windows\\system32\\notepad.exe",f_txt])
 		command = get_audio()
-		if "yes" in command:
+		if re.search(".*yes|yep.*",command):
 			x.kill()
+		elif re.search(".*no|not|wrong.*",command):
+			talk_to_me("Please make the necessary changes in it")
 
 
-	elif re.search(".*active.*",command):
-		x = covid.get_total_active_cases()
-		talk_to_me(f"The  number of active covid-19 cases  are {x} ")
 
-	elif re.search(".*confirmed.*",command):
-		x = covid.get_total_confirmed_cases()
-		talk_to_me(f"The  number of confirmed covid-19 cases are {x} ")	
+	elif re.search(".*active|recovered|deaths|confirmed.*",command):
+		type_c = re.findall(".*(active|recovered|deaths|confirmed).*",command)
+		#print(type_c)
+		command = command.split(" ")
+		if command[-1] == "world":
+	 		#print("world")
+	 		x = covid.get_total_active_cases()
+		else:
+	# 		print(command[-1])
+	 		x = covid.get_status_by_country_name(command[-1])
+	 		x = x[type_c[0]]
+			
+		talk_to_me(f"The  number of {type_c[0]} covid-19 cases in {command[-1]} are {x} ")
 
-	elif re.search(".*recovered|recoveries.*",command):
-		x = covid.get_total_recovered()
-		talk_to_me(f"The  number of recovered covid-19 cases  are {x} ")
+	# elif re.search(".*confirmed.*",command):
+	# 	x = covid.get_total_confirmed_cases()
+	# 	talk_to_me(f"The  number of confirmed covid-19 cases are {x} ")	
 
-	elif re.search(".*deaths.*",command):
-		x = covid.get_total_deaths()
-		talk_to_me(f"The  number of deaths due to  covid-19 are {x} ")
+	# elif re.search(".*recovered|recoveries.*",command):
+	# 	x = covid.get_total_recovered()
+	# 	talk_to_me(f"The  number of recovered covid-19 cases  are {x} ")
+
+	# elif re.search(".*deaths.*",command):
+	# 	x = covid.get_total_deaths()
+	# 	talk_to_me(f"The  number of deaths due to  covid-19 are {x} ")
+
+	else:	
+				driver = webdriver.Chrome()
+				driver.get("https://www.google.com/search?q="+ command)	
 
 		
 
